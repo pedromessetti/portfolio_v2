b64293ed3ce3a5be95428b87a48b109c4610ccfd
Pedro Messetti <105685220+pedromessetti@users.noreply.github.com>
2023-03-12 10:13:43 +0000

    Atualizar o index.tsx

--- portfolio/src/components/StudiesLogo/index.tsx
+++ portfolio/src/components/StudiesLogo/index.tsx
@@ -5,14 +5,15 @@ import style from './StudiesLogo.module.scss';
 interface Props {
 	name: string,
 	path: string,
-	id: string
+	id: string,
+ href: string
 }
 
 export default function StudiesLogo(props: Props) {
 	return (
-		<div className={style.container}>
+		<a href={props.href} target='_blank' rel='noreferrer' className={style.container}>
 			<img src={props.path} alt={`${props.name} Logo`} />
 			<p>{props.name}</p>
-		</div>
+		</a>
 	);
 }
