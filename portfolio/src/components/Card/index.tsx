3b68492b4f329fdd8094682d9c0de36c834ce290
pedromessetti <pedromessetti@gmail.com>
2023-07-10 16:39:44 +0100

    add wpp button

--- portfolio/src/components/Card/index.tsx
+++ portfolio/src/components/Card/index.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import styles from './Card.module.scss';
-import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaTwitter, FaWhatsapp, FaInstagram } from 'react-icons/fa';
+import { ImWhatsapp } from 'react-icons/im';
 import image from 'assets/myself/pedro.jpeg';
 
 
 export default function Card() {
-	const icon = <FaTwitter />;
 	return (
 		<div className={styles.card}>
 			<div className={styles.imageContainer}>
 				<img src={image} alt="Pedro Messetti" className={styles.image} />
 				<div className={styles.overlay}></div>
 				<div className={styles.socialIcons}>
-					<a href="google.com" target="_blank" rel="noopener noreferrer">
-						{icon}
+					<a href="https://api.whatsapp.com/send?phone=351924686213" target="_blank" rel="noopener noreferrer">
+						<ImWhatsapp />
 					</a>
-					<a href="google.com" target="_blank" rel="noopener noreferrer">
-						<FaFacebook />
-					</a>
-					<a href="google.com" target="_blank" rel="noopener noreferrer">
+					<a href="https://www.instagram.com/pedromessetti/" target="_blank" rel="noopener noreferrer">
 						<FaInstagram />
+					</a>
+					<a href="https://twitter.com/pedro_messetti" target="_blank" rel="noopener noreferrer">
+						<FaTwitter />
 					</a>
 				</div>
 			</div>
